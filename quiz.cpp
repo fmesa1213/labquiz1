50ed1852af7adbde6b006a99a5c654d5e75f4e7d
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2023-01-30 17:45:54 -0500

    Create quiz.cpp

---
+++ quiz.cpp
@@ -0,0 +1,19 @@
+//Frida Mesa 1/30/23
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int num;
+int i;
+
+cout << "Enter a number:" << endl;
+cin >> num;
+
+for (i = 0; i < num; i++)
+{
+cout << "I love C++" << endl;
+}
+ return 0;
+}
